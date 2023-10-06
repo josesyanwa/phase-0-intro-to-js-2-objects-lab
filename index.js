05226ab1833c2205dc3b4a5f1a7b77c00305fb84
Joses Syanwa <josesyanwa@gmail.com>
2023-10-06 12:41:08 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Write your solution in this file!
+// index.js
+
+// Function to update an employee with a new key-value pair (non-destructive)
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+  const newEmployee = { ...employee };
+  newEmployee[key] = value;
+  return newEmployee;
+}
+
+// Function to update an employee with a new key-value pair (destructive)
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+  employee[key] = value;
+  return employee;
+}
+
+// Function to delete a key from a clone of an employee (non-destructive)
+function deleteFromEmployeeByKey(employee, key) {
+  const newEmployee = { ...employee };
+  delete newEmployee[key];
+  return newEmployee;
+}
+
+// Function to delete a key from an employee (destructive)
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+  delete employee[key];
+  return employee;
+}
+
+
+
+
